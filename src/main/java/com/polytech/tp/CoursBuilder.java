2f4337be6dcf5611ce4a0f222f25e764c255e8cd
Fares <yacinesisou18@gmail.com>
2025-11-20 14:12:56 +0100

    Builder + Observer

--- src/main/java/com/polytech/tp/CoursBuilder.java
+++ src/main/java/com/polytech/tp/CoursBuilder.java
@@ -2,13 +2,69 @@ package com.polytech.tp;
 
 public class CoursBuilder {
     // TODO: Implémenter le pattern Builder
-    
-    
-    public CoursBuilder setMatiere(String matiere) { return this; }
-    public CoursBuilder setEnseignant(String enseignant) { return this; }
-    
-    
-    public Cours build() {
-        return null;
+    private String matiere;
+    private String enseignant;
+    private String salle;
+    private String date;
+    private String heureDebut;
+    private boolean estOptionnel;
+    private String niveau;
+    private boolean necessiteProjecteur;
+
+    public CoursBuilder setMatiere(String matiere) {
+        this.matiere = matiere;
+        return this;
     }
+
+    public CoursBuilder setEnseignant(String enseignant) {
+        this.enseignant = enseignant;
+        return this;
+    }
+
+    public CoursBuilder setSalle(String salle) {
+        this.salle = salle;
+        return this;
+    }
+
+    public CoursBuilder setDate(String date) {
+        this.date = date;
+        return this;
+    }
+
+    public CoursBuilder setHeureDebut(String heureDebut) {
+        this.heureDebut = heureDebut;
+        return this;
+    }
+
+    public CoursBuilder setEstOptionnel(boolean estOptionnel) {
+        this.estOptionnel = estOptionnel;
+        return this;
+    }
+
+    public CoursBuilder setNiveau(String niveau) {
+        this.niveau = niveau;
+        return this;
+    }
+
+    public CoursBuilder setNecessiteProjecteur(boolean necessiteProjecteur) {
+        this.necessiteProjecteur = necessiteProjecteur;
+        return this;
+    }
+
+    public Cours build() {
+        return new Cours(
+                this.matiere,
+                this.enseignant,
+                this.salle,
+                this.date,
+                this.heureDebut,
+                this.estOptionnel,
+                this.niveau,
+                this.necessiteProjecteur
+        );
+    }
+
+    
+    
+
 }
--- src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
+++ src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
@@ -3,23 +3,44 @@ package com.polytech.tp;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GestionnaireEmploiDuTemps {
+public class GestionnaireEmploiDuTemps implements Subject {
+    private  List<Observer> listObservers = new ArrayList<>();
     private List<ICours> listeCours = new ArrayList<>();
+
+
+    public void attach(Observer o) {
+        listObservers.add(o);
+    }
+
+
+    public void detach(Observer o) {
+        listObservers.remove(o);
+    }
+
+
+    public void notifyObservers(String message) {
+        for (Observer o : listObservers) {
+            o.update(message);
+        }
+    }
 
     public void ajouterCours(ICours cours) {
         this.listeCours.add(cours);
         System.out.println("Nouveau cours ajouté : " + cours.getDescription());
         // TODO: C'est ici qu'il faudrait notifier les étudiants (Observer pattern)
+        notifyObservers("Nouveau cours ajouté : " + cours.getDescription());
     }
 
     public void modifierCours(ICours cours, String message) {
         // Logique de modification...
         System.out.println("Cours modifié : " + message);
         // TODO: Notifier les observateurs ici aussi
+        notifyObservers(message);
     }
 
     public void setChangement(String string) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setChangement'");
+        notifyObservers(string);
+       // throw new UnsupportedOperationException("Unimplemented method 'setChangement'");
     }
 }
